e3b0464c825b445062630ba5df7cc9e9597e74f0
profitweb <chezsimo@yandex.ru>
2023-02-09 13:18:17 +0300

    condition /2

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        while(true) {
+            condition();
+        }
+        }
+
+    private static void condition() {
+        System.out.println(inputNumber() % 2 == 0);
+    }
+
+    private static int inputNumber() {
+        System.out.println("Please enter any number");
+        return new Scanner(System.in).nextInt();
     }
 }
